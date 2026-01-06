f814e33111c6d3c297354bfff7ffead5effaa2d4
sagtecnologia <bruno.alencar@sagtecnologia.com.br>
2026-01-06 15:34:50 -0300

    Fix: Usar chave correta (sb_publishable)

--- config/supabase.js
+++ config/supabase.js
@@ -3,7 +3,7 @@
 // ================================
 
 const SUPABASE_URL = 'https://ekpswrbjaflquxmacsmq.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrcHN3cmJqYWZscXV4bWFjc21xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY4MTI4NTIsImV4cCI6MjA1MjM4ODg1Mn0.4FJVcwYEb-C1jkUwEtVzx7XrgHAoJH84ypj5n4RvqVA';
+const SUPABASE_ANON_KEY = 'sb_publishable_4ASaBVQDA95x9zJF302enA_MeAshTRf';
 
 // Inicializar cliente Supabase
 window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
